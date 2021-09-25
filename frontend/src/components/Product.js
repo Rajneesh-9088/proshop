e3ba4a4d7abbb179898a68956fa2ec1cd15da970
Rajneesh-9088 <rp8735685@gmail.com>
2021-09-26 01:39:44 +0530

    rating components

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { Card, CardGroup, CarouselItem } from 'react-bootstrap'
+import { Card} from 'react-bootstrap'
+import Rating from './Rating';
+
 
 const Product = ({product}) => {
+    
     return (
         <Card className='my-3 p-3 rounded'>
             <a href={`/product/${product._id}`}>
@@ -13,9 +16,9 @@ const Product = ({product}) => {
                 </a>
             </Card.Body>
             <Card.Text as='div'>
-                <div className="my-3">
-                   {product.rating} from {product.numReviews} reviews
-                </div>
+               <Rating value={product.rating}
+                   text={`${product.numReviews} reviews`} 
+                />
             </Card.Text>
             <Card.Text as='h3'>
                ${product.price}
