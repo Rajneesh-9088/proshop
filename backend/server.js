52325d1372e677507154331e29970271443eed59
Rajneesh-9088 <rp8735685@gmail.com>
2021-10-01 20:52:05 +0530

    custom middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,30 @@
-import express from  "express"
-import dotenv from  'dotenv'
-import colors from 'colors'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
 import connectDB from "./config/db.js";
-import products from './data/products.js'
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
-
-
-dotenv.config()
+dotenv.config();
 const app = express();
 
-connectDB()
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get('/api/products', (req,res)=>{
-  res.json(products)
-})
+app.use("/api/products", productRoutes);
 
-app.get('/api/products/:id', (req,res)=>{
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-  })
+app.use(notFound)
 
-const PORT = process.env.PORT || 5000  
+app.use(errorHandler)
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
