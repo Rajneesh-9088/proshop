e5dc185cccce42ed8822097f02ce2e51ece29fb4
Rajneesh-9088 <rp8735685@gmail.com>
2021-10-02 01:26:57 +0530

    single product screen state manage by redux

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -6,13 +6,12 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { listProducts } from "../actions/productActions";
 
-
 const HomeScreen = () => {
   const dispatch = useDispatch();
 
   const productList = useSelector((state) => state.productList);
   const { loading, error, products } = productList;
-
+  
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
@@ -21,9 +20,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-       <Loader />  
+        <Loader />
       ) : error ? (
-        <Message variant='danger'>{error}</Message>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
