5027ce542000d9be8925a737fce0442fe39208a2
Rajneesh-9088 <rp8735685@gmail.com>
2021-10-18 02:51:25 +0530

    Integrating PayPal Payments

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,10 +33,9 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
     const createdOrder = await order.save();
 
-    res.status(201).json(createdOrder);
+   res.status(201).json(createdOrder);
   }
 
-  res.json(products);
 });
 
 // @desc   Get order by id
@@ -55,4 +54,29 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById };
+// @desc   Update order to paid
+// @route  GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+  
+  if (order) {
+    order.isPaid = true,
+    order.paidAt = Date.now()
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address
+    }
+   
+    const updatedOrder = await order.save()
+    res.json(updatedOrder)
+
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+export { addOrderItems, getOrderById,updateOrderToPaid };
--- frontend/src/screen/OrderScreen.js
+++ frontend/src/screen/OrderScreen.js
@@ -1,19 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { PayPalButton } from "react-paypal-button-v2";
 import { Link } from "react-router-dom";
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { getOrderDetails } from "../actions/orderActions";
+import { getOrderDetails, payOrder } from "../actions/orderActions";
+import { ORDER_PAY_FAIL, ORDER_PAY_RESET } from "../constants/orderConstants";
 
 const OrderScreen = ({ match }) => {
   const orderId = match.params.id;
+
+  const [sdkReady, setSdkReady] = useState(false);
 
   const dispatch = useDispatch();
 
   const orderDetails = useSelector((state) => state.orderDetails);
 
   const { order, loading, error } = orderDetails;
+
+  const orderPay = useSelector((state) => state.orderPay);
+
+  const { loading: loadingPay, success: successPay } = orderPay;
 
   if (!loading) {
     // Calculate Prices
@@ -27,8 +36,36 @@ const OrderScreen = ({ match }) => {
   }
 
   useEffect(() => {
-    dispatch(getOrderDetails(orderId));
-  }, []);
+    const addPayPalScript = async () => {
+      const { data: clientId } = await axios.get("/api/config/paypal");
+      const script = document.createElement("script");
+      script.type = "text/javascript";
+      script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
+      script.async = true;
+      script.onload = () => {
+        setSdkReady(true);
+      };
+      document.body.appendChild(script);
+    };
+
+    // dispatch(getOrderDetails(orderId));
+    if (!order || successPay || order._id !== orderId) {
+      dispatch({type: ORDER_PAY_RESET})
+      dispatch(getOrderDetails(orderId));
+    } else if (!order.isPaid) {
+      if (!window.paypal) {
+        addPayPalScript();
+      } else {
+        setSdkReady(true);
+      }
+    }
+  }, [dispatch, successPay, order, orderId]);
+
+  const successPaymentHandler = (paymentResult) =>{
+    console.log("raj");
+     console.log(paymentResult)
+     dispatch(payOrder(orderId,paymentResult))
+  }
 
   return loading ? (
     <Loader />
@@ -56,7 +93,10 @@ const OrderScreen = ({ match }) => {
                 {order.shippingAddress.country}
               </p>
               {order.isDelivered ? (
-                <Message variant="success"> Delivered on {order.deliveredAt} </Message>
+                <Message variant="success">
+                  {" "}
+                  Delivered on {order.deliveredAt}{" "}
+                </Message>
               ) : (
                 <Message variant="danger">Not Delivered</Message>
               )}
@@ -142,6 +182,19 @@ const OrderScreen = ({ match }) => {
                   <Col>${order.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
+              {!order.isPaid && (
+                <ListGroup.Item>
+                  {loadingPay && <Loader />}
+                  {!sdkReady ? (
+                    <Loader />
+                  ) : (
+                    <PayPalButton
+                      amount={order.totalPrice}
+                      onSuccess={successPaymentHandler}
+                    />
+                  )}
+                </ListGroup.Item>
+              )}
             </ListGroup>
           </Card>
         </Col>
