aad1df83b03d75328a7f7a391b79b210a07f0b4d
Rajneesh-9088 <rp8735685@gmail.com>
2021-10-19 02:19:02 +0530

    show orders on profile

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,7 +13,7 @@ import {
   userUpdateProfileReducer,
 } from "./reducers/userReducers";
 
-import { orderCreateReducer, orderDetailsReducer,orderPayReducer } from "./reducers/orderReducers";
+import { orderCreateReducer, orderDetailsReducer,orderPayReducer,orderListMyReducer } from "./reducers/orderReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -25,7 +25,8 @@ const reducer = combineReducers({
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
-  orderPay: orderPayReducer
+  orderPay: orderPayReducer,
+  orderListMy: orderListMyReducer
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
