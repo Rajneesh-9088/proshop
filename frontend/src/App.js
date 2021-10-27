2bb720e141cf4c0a0ee2c66fc3a35168a03c0df2
Rajneesh-9088 <rp8735685@gmail.com>
2021-10-27 17:48:15 +0530

    admin order list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import UserListScreen from "./screen/UserListScreen";
 import UserEditScreen from "./screen/UserEditScreen";
 import ProductListScreen from "./screen/ProductListScreen";
 import ProductEditScreen from "./screen/ProductEditScreen";
+import OrderListScreen from "./screen/OrderListScreen";
 
 const App = () => {
   return (
@@ -37,6 +38,7 @@ const App = () => {
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+          <Route path="/admin/orderlist" component = {OrderListScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
