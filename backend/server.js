67c155d07a218bf9ef9999fd8edc1dfb5095b662
Rajneesh-9088 <rp8735685@gmail.com>
2021-10-31 01:35:29 +0530

    backend functionality to add product reviews

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
+import morgan from 'morgan';
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
@@ -15,6 +16,10 @@ import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config();
 const app = express();
+
+if(process.env.NODE_ENV  == 'development'){
+      app.use(morgan('dev'));
+}
 
 app.use(express.json())  // to accept json data in body
 
