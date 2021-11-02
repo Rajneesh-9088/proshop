c9d0b8ee50aa31e5a5f26bd7db942699564d8c90
Rajneesh-9088 <rp8735685@gmail.com>
2021-11-03 04:01:16 +0530

    deployment to heroku ready

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,55 @@
-import path from 'path'
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import morgan from 'morgan';
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import orderRoutes from './routes/orderRoutes.js'
-import uploadRoutes from './routes/uploadRoutes.js'
-
-
-
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 const app = express();
 
-if(process.env.NODE_ENV  == 'development'){
-      app.use(morgan('dev'));
+if (process.env.NODE_ENV == "development") {
+  app.use(morgan("dev"));
 }
 
-app.use(express.json())  // to accept json data in body
+app.use(express.json()); // to accept json data in body
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes)
-app.use("/api/orders",orderRoutes)
-app.use('/api/upload',uploadRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 
-app.get('/api/config/paypal', (req,res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+app.use(notFound);
 
-app.use(notFound)
-
-app.use(errorHandler)
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
